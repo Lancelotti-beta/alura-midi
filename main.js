743deb70151640a6027e28723a651fb261896f2e
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-05-22 19:27:02 -0300

    Parte II - Criando 'function', usando 'onclick'

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-const pom = document.getElementById('som_tecla_pom');
+function tocaSomDePom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+//const pom = document.getElementById('som_tecla_pom');
 const btn_pom = document.querySelector('.tecla_pom');
 
-btn_pom.addEventListener('click', () => {
-    alert('Pom!');
-});
+btn_pom.onclick = tocaSomDePom;
+
+/*
+const listaTeclado = document.querySelector('.teclado');
+for(let i = 0; i < btn_pom.length; i++){
+    btn_pom[i].addEventListener('click', () => {
+        document.querySelector('#som_tecla_pom').play();
+    });
+
+}
+*/
+
+
