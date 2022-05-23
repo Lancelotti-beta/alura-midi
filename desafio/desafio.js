f65fd87c4120ed93fba8f81a7b862786eedd821d
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-05-23 13:12:38 -0300

    Tentando resolver o problema - 50% em andamento

--- desafio/desafio.js
+++ desafio/desafio.js
@@ -1,15 +1,20 @@
 const listaTeclado = document.querySelector('.teclado');
 const btn = listaTeclado.querySelectorAll('input[type="button"]');
+let tel = document.querySelector('input[type="tel"]');
 
-const tel = document.querySelector('input[type="tel"]');
-//console.log(tel);
+function imprimeNaTela(n){
+	let valor = tel.value;
+	//até aqui eu tenho o 'placeholder' com o valor do 'botão'
+	valor = n;
+}
 
 for(let i = 0; i < btn.length; i++){
 	let valorAtualDoBtn = btn[i].value;
 	btn[i].addEventListener('click', () => {
 		//Até aqui eu já pego o valor do input
-		console.log(valorAtualDoBtn);
+		//console.log(valorAtualDoBtn);
 
+		imprimeNaTela(valorAtualDoBtn);
 	});
 }
 
