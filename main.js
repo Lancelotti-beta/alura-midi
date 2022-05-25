eafda75ea305ee96af1e77f95258fac14f6c8521
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-05-25 14:06:01 -0300

    AluraMidi| duas resoluções - uma rude(minha) outra eficaz(Professora)

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-function tocaSomDePom (i) {
-    document.querySelectorAll('.som')[i].play();
+function tocaSom(idConcatenado) {
+    //document.querySelectorAll('.som')[i].play(); => Minha resolução, bem amadora.
+    document.querySelector(idConcatenado).play();
 }
 
 const btn = document.querySelectorAll('.tecla');
 
 for(let i = 0; i < btn.length; i++){
-    btn[i].addEventListener('click', () => {
-        tocaSomDePom(i);
-    });
+    const tecla = btn[i];
+    const classeBotao = tecla.classList[1];
 
+    const instrumento = `#som_${classeBotao}`;
+
+    /* inha resolução, bem rude creio eu.
+    btn[i].addEventListener('click', () => {
+        tocaSom(i);
+    });
+    */
+
+    tecla.onclick = function () {
+        tocaSom(instrumento);
+    };
 }
 
 
